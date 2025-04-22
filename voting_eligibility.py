5a8958fc41e81492b281a0e534639050f185bd9e
cypher-max <mahbubalam3937@gmail.com>
2025-04-22 12:55:20 +0600

    Initial commit

--- voting_eligibility.py
+++ voting_eligibility.py
@@ -3,8 +3,10 @@ age= int(input ("How old are you? "))
 
 if  age>=18 and age <=24:
     print (f"{nam}, you are eligible to vote")
-elif age >= 25:
+elif age >= 25 and age <= 120:
     print (f"{nam}, you are eligible to vote and run for parliament")
+elif age <= 0 or age > 120:
+    print (f"{nam}, please enter a valid age")
 else :
     age = 18 - age
     print (f"{nam}, you are a minor and have to wait {age} years to vote")
